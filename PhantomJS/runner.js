a51334c466f9d9213a1828792983e1b4fbff8458
Brett Bukowski <brett.bukowski@gmail.com>
2013-08-31 12:31:58 -0600

    Display path to elements in PhantomJS reporter

--- PhantomJS/runner.js
+++ PhantomJS/runner.js
@@ -14,9 +14,19 @@ var HTMLCS_RUNNER = new function() {
         });
     };
 
+    this.identifyNode = function(node) {
+        if (!node || !node.tagName) return [];
+
+        var id = node.id ? '#' + node.id : '',
+            className = node.className ? '.' + node.className.split(' ').join('.') : '';
+
+        return this.identifyNode(node.parentNode).concat([node.tagName + id + className]);
+    };
+
     this.output = function(msg) {
         // Simple output for now.
         var typeName = 'UNKNOWN';
+        var pathToElement = this.identifyNode(msg.element).join(' > ');
         switch (msg.type) {
             case HTMLCS.ERROR:
                 typeName = 'ERROR';
@@ -31,7 +41,7 @@ var HTMLCS_RUNNER = new function() {
             break;
         }//end switch
 
-        console.log(typeName + '|' + msg.code + '|' + msg.msg);
+        console.log([typeName, msg.code, msg.msg, pathToElement].join('|'));
     };
 
 };
